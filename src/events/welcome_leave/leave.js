d910977d74e728886f5f3e73b353915a30a773aa
Blank00200 <raghavraturi8c@gmail.com>
2025-03-10 13:56:46 +0000

    automatic

--- src/events/welcome_leave/leave.js
+++ src/events/welcome_leave/leave.js
@@ -2,18 +2,17 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = (client) => {
     client.on('guildMemberRemove', async (member) => {
-        const channel = member.guild.systemChannel; // Change if needed
+        const channel = member.guild.systemChannel; // Change this if needed
         if (!channel) return;
 
-        const leaveEmbed = new EmbedBuilder()
-            .setColor('#ff0000') // Red color
-            .setTitle(`💔 Goodbye from Endless Void`)
-            .setDescription(`Sad to see you go, **${member.user.username}**. Hope to see you again!`)
-            .setThumbnail('https://i.imgur.com/9MkM6Kq.png') // Change thumbnail if needed
-            .setFooter({ text: 'Endless Void Community', iconURL: member.guild.iconURL() })
-            .setTimestamp()
-            .setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL({ dynamic: true }) });
+        const embed = new EmbedBuilder()
+            .setColor('#ff0000')
+            .setTitle('😢 Goodbye!')
+            .setDescription(`**${member.user.tag}** has left **Endless Void**. We'll miss you! 💔`)
+            .setThumbnail('https://i.imgur.com/Fn4RjYw.png') // Change this URL for a custom thumbnail
+            .setFooter({ text: `We now have ${member.guild.memberCount} members.`, iconURL: member.user.displayAvatarURL() })
+            .setTimestamp();
 
-        channel.send({ embeds: [leaveEmbed] });
+        channel.send({ embeds: [embed] });
     });
 };
--- src/events/welcome_leave/welcome.js
+++ src/events/welcome_leave/welcome.js
@@ -2,19 +2,18 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = (client) => {
     client.on('guildMemberAdd', async (member) => {
-        const channel = member.guild.systemChannel; // Change if needed
+        const channel = member.guild.systemChannel; // Change this if needed
         if (!channel) return;
 
-        const welcomeEmbed = new EmbedBuilder()
-            .setColor('#00ff00') // Green color
-            .setTitle(`👋 Welcome to Endless Void!`)
-            .setDescription(`Hey ${member}, welcome to **Endless Void**! 🎉\nMake sure to check out the rules and have a great time!`)
-            .setThumbnail('https://i.imgur.com/VB6ETtM.png') // Change thumbnail if needed
-            .setImage('https://i.imgur.com/dfHytFL.jpg') // Change or remove background image
-            .setFooter({ text: 'Endless Void Community', iconURL: member.guild.iconURL() })
-            .setTimestamp()
-            .setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL({ dynamic: true }) });
+        const embed = new EmbedBuilder()
+            .setColor('#00ff00')
+            .setTitle('🎉 Welcome to Endless Void!')
+            .setDescription(`Hey ${member}, welcome to **Endless Void**! 🎊\nHope you have a great time here!`)
+            .setThumbnail('https://i.imgur.com/Fn4RjYw.png') // Change this URL for a custom thumbnail
+            .setImage('https://i.imgur.com/zM1l4j5.gif') // Optional banner image
+            .setFooter({ text: `Member #${member.guild.memberCount}`, iconURL: member.user.displayAvatarURL() })
+            .setTimestamp();
 
-        channel.send({ embeds: [welcomeEmbed] });
+        channel.send({ embeds: [embed] });
     });
 };
